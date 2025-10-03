8ec00e92ee84832dada945178f3221cc0c17dd5c
Osva-Dev <osvadev30@gmail.com>
2025-10-02 23:17:30 -0600

    First Algorithm

---
+++ algorithms/script.js
@@ -0,0 +1,24 @@
+// ¿Cuál número es mayor entre dos números?
+
+const firstNumber = Number(prompt("Ingresa el primer número"));
+const secondNumber = Number(prompt("Ingresa el segundo número"));
+
+function whichNumberIsGreater(x, y) {
+  if (isNaN(x) || isNaN(y)) {
+    return "Por favor ingresa solo números válidos.";
+  }
+
+  if (x == " " || y == " ") {
+    return "Porfavor no deje vacio las peticiones";
+  }
+
+  if (x === y) {
+    return "Ambos números son iguales.";
+  } else if (x > y) {
+    return "El primer número es mayor.";
+  } else {
+    return "El segundo número es mayor.";
+  }
+}
+
+alert(whichNumberIsGreater(firstNumber, secondNumber));
